4f7a53b1cecb5ef76e35363d8b531dc0f573473f
deamyasin <73380813+deamyasin@users.noreply.github.com>
2021-06-24 20:59:00 +0700

    update lagi

--- client_react/src/HomePage/HomePage.jsx
+++ client_react/src/HomePage/HomePage.jsx
@@ -19,10 +19,10 @@ function HomePage() {
 
     return (
         <div className="col-lg-8 offset-lg-2">
-            <h1>Hi {user.firstName}!</h1>
-            <p>You're logged in with React Hooks!!</p>
-            <h3>All registered users:</h3>
-            {users.loading && <em>Loading users...</em>}
+            <h1>Hiii !!! {user.firstName}!</h1>
+            <p>Kamu Berhasil Log ini .. Hore !!</p>
+            <h3>Mahasiswa yang telah Log In</h3>
+            {users.loading && <em>Menunggu ...</em>}
             {users.error && <span className="text-danger">ERROR: {users.error}</span>}
             {users.items &&
                 <ul>
@@ -30,16 +30,16 @@ function HomePage() {
                         <li key={user.id}>
                             {user.firstName + ' ' + user.lastName}
                             {
-                                user.deleting ? <em> - Deleting...</em>
+                                user.deleting ? <em> - Hapus...</em>
                                 : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                : <span> - <a onClick={() => handleDeleteUser(user.id)} className="text-primary">Delete</a></span>
+                                : <span> - <a onClick={() => handleDeleteUser(user.id)} className="text-primary">Hapus</a></span>
                             }
                         </li>
                     )}
                 </ul>
             }
             <p>
-                <Link to="/login">Logout</Link>
+                <Link to="/login">Keluar</Link>
             </p>
         </div>
     );
